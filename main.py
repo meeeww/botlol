0308f4f1e1ad5dbddcd1b0814cd6c7e899ec8e3e
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-02-07 17:56:16 +0100

    zas

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pyautogui
+import time
+import os
 
 """
 1. python.exe -m pip install --upgrade pip
@@ -8,7 +10,17 @@ import pyautogui
 """
 
 location = None
-imageFile = 'Captura.PNG'
+imageFile = 'VisualStudio.PNG'
+chat_list = []
+directory = os.listdir()
+imageChat = "Chat.PNG"
+imageChatSecond = "Chat2.PNG"
+imageChatBubble = "Omar.PNG"
+
+
+for file in directory:
+    if file.startswith('Chat'):
+        chat_list.append(file)
 
 while (location == None):
     try:
@@ -17,3 +29,36 @@ while (location == None):
         print(e)
 
 print(location)
+pyautogui.moveTo(location, duration=1)
+pyautogui.click()
+pyautogui.click()
+location = None
+
+
+while (location == None):
+    for image in chat_list:
+        print(image)
+        location = pyautogui.locateOnScreen(image)
+        if(location != None):
+            print("salimos")
+            break
+
+
+print(location)
+pyautogui.moveTo(location, duration=1)
+pyautogui.click()
+location = None
+
+while (location == None):
+    try:
+        location = pyautogui.locateOnScreen(imageChatBubble)
+    except Exception as e:
+        print(e)
+
+print(location)
+pyautogui.moveTo(location, duration=1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.write("Hola!!!")
+pyautogui.press('enter')
+location = None
