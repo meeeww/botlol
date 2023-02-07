231c8a7dfe9b0ec1b15752151990f008e0ffb06d
DESKTOP-C9HHN7V\A8-PC00 <licencias@svalero.com>
2023-02-07 19:33:51 +0100

    zas

--- main.py
+++ main.py
@@ -1,48 +1,50 @@
 import pyautogui
 import time
 import os
+import colorama
+from colorama import Back, Fore, Style
+colorama.init(autoreset=True)
+
 
 """
 1. python.exe -m pip install --upgrade pip
 2. pip install Pillow --upgrade
 3. pip install pyautogui
+4. pip install colorama
+
+auto-py-to-exe -> para hacer deploy de .exe
 
 """
 
+print("hello")
 location = None
-imageFile = 'VisualStudio.PNG'
 chat_list = []
 directory = os.listdir()
-imageChat = "Chat.PNG"
-imageChatSecond = "Chat2.PNG"
+#imageChat = "Chat.PNG"
+#imageChatSecond = "Chat2.PNG"
 imageChatBubble = "Omar.PNG"
+print(Back.GREEN + " aqui")
+print(Fore.RED + " hey")
 
 
 for file in directory:
-    if file.startswith('Chat'):
+    if file.startswith('Ch'):
         chat_list.append(file)
 
-while (location == None):
-    try:
-        location = pyautogui.locateOnScreen(imageFile)
-    except Exception as e:
-        print(e)
-
-print(location)
-pyautogui.moveTo(location, duration=1)
-pyautogui.click()
-pyautogui.click()
-location = None
-
+print("xf")
 
 while (location == None):
+    print("brooo")
     for image in chat_list:
+        print("test")
         print(image)
         location = pyautogui.locateOnScreen(image)
-        if(location != None):
+        print(location)
+        if (location != None):
             print("salimos")
             break
 
+print("aqui2")
 
 print(location)
 pyautogui.moveTo(location, duration=1)
