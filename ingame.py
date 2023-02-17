03db2cd69989d21b4ba6936fde3099f8109a1402
mew <102923191+meeeww@users.noreply.github.com>
2023-02-17 12:52:09 +0100

    zas

--- ingame.py
+++ ingame.py
@@ -4,31 +4,48 @@ import pyautogui as py
 import math
 import time
 
-
-def wait(segundos):
-    time.sleep(segundos)
-
-
-colorJugadorLower = np.array([20, 150, 20])
-colorJugadorUpper = np.array([25, 255, 255])  # jugador
-colorEnemigoLower = np.array([2, 130, 20])
-colorEnemigoUpper = np.array([5, 255, 255])  # ENEMIGOS,
-
 centroPantalla = (930, 500)
 
-img = cv2.imread("hey.png")
-global enemigosCoordenadas
-enemigosCoordenadas = []
-global jugadorCoordenada
-jugadorCoordenada = (1, 5)
+def wait(segundos):
+        time.sleep(segundos)
 
+def kitear():
+        wait(0.5)
+        py.moveTo(centroPantalla[0] + 50, centroPantalla[1])
+        py.mouseDown(button='right')
+        py.mouseUp(button='right')
+        wait(0.5)
+        py.moveTo(centroPantalla[0] - 150, centroPantalla[1])
+        py.mouseDown(button='right')
+        py.mouseUp(button='right')
+        cv2.waitKey()
 
-def jugador():
+def inGameController():
+    colorJugadorLower = np.array([23, 150, 180])
+    colorJugadorUpper = np.array([25, 225, 255]) # jugador
+    colorEnemigoLower = np.array([3, 144, 153])
+    colorEnemigoUpper = np.array([3, 210, 206])# ENEMIGOS,
+    colorMinionLower = np.array([0, 130, 100])
+    colorMinionUpper = np.array([1, 145, 255])# MINIONS,
+
+    global jugadorCoordenada
+    global enemigosCoordenadas
+
+    enemigoDetectado = False
+
+    atacar = []
+    farmear = []
+    enemigosCoordenadas = []
+    minionsCoordenadas = []
+    jugadorCoordenada = (1, 5)
+    #py.screenshot().save("hey.png")
+    img = cv2.imread("hey.png")
+
+    #--------------------------------------------------------------conseguir jugador--------------------------------------------------------------
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(image, colorJugadorLower, colorJugadorUpper)
 
-    contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
         for contour in contours:
@@ -37,82 +54,88 @@ def jugador():
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
                 global jugadorCoordenada
                 jugadorCoordenada = (x, y)
-
-
-def enemigo():
+    #--------------------------------------------------------------conseguir enemigos--------------------------------------------------------------
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(image, colorEnemigoLower, colorEnemigoUpper)
 
-    contours, hierarchy = cv2.findContours(
-        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     if len(contours) != 0:
         for contour in contours:
-            if cv2.contourArea(contour) > 500:
+            if cv2.contourArea(contour) > 50:
                 x, y, w, h = cv2.boundingRect(contour)
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
-                global enemigosCoordenadas
                 enemigosCoordenadas.append([x, y])
+    
+    for coordenada in enemigosCoordenadas:
+        primerPuntoX = coordenada[0]
+        primerPuntoY = coordenada[1]
+        segundoPuntoX = jugadorCoordenada[0]
+        segundoPuntoY = jugadorCoordenada[1]
 
+        distancia = math.sqrt(math.pow((segundoPuntoX-primerPuntoX), 2) + math.pow((segundoPuntoY-primerPuntoY), 2))
+        #print(str(distancia) + " distancia")
+        if distancia < (525*0.6):
+            cv2.line(img, (primerPuntoX + 50, primerPuntoY + 100), (segundoPuntoX + 50, segundoPuntoY + 120), (0, 255, 0), 2)
+            atacar.append([primerPuntoX + 100, primerPuntoY + 150, distancia])
+            enemigoDetectado = False
+        else:
+            cv2.line(img, (primerPuntoX + 50, primerPuntoY + 100), (segundoPuntoX + 50, segundoPuntoY + 120), (0, 0, 255), 2)
 
-def kitear():
-    wait(0.5)
-    py.moveTo(centroPantalla[0] + 50, centroPantalla[1])
-    py.mouseDown(button='right')
-    py.mouseUp(button='right')
-    wait(0.5)
-    py.moveTo(centroPantalla[0] - 50, centroPantalla[1])
-    py.mouseDown(button='right')
-    py.mouseUp(button='right')
+    #--------------------------------------------------------------conseguir minions--------------------------------------------------------------
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(image, colorMinionLower, colorMinionUpper)
+
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) != 0:
+        for contour in contours:
+            if cv2.contourArea(contour) > 0:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
+                minionsCoordenadas.append([x, y])
+    
+    for coordenada in minionsCoordenadas:
+        primerPuntoX = coordenada[0]
+        primerPuntoY = coordenada[1]
+        segundoPuntoX = jugadorCoordenada[0]
+        segundoPuntoY = jugadorCoordenada[1]
+
+        distancia = math.sqrt(math.pow((segundoPuntoX-primerPuntoX), 2) + math.pow((segundoPuntoY-primerPuntoY), 2))
+        #print(str(distancia) + " distancia")
+        if distancia < (525*0.6):
+            mitad = math.floor(x + h)
+            cv2.line(img, (primerPuntoX + 25, primerPuntoY + 40), (segundoPuntoX + 50, segundoPuntoY + 120), (0, 255, 255), 2)
+            farmear.append([primerPuntoX + 100, primerPuntoY + 150, distancia])
+            enemigoDetectado = False
+        else:
+            cv2.line(img, (primerPuntoX + 25, primerPuntoY + 40), (segundoPuntoX + 50, segundoPuntoY + 120), (0, 0, 255), 2)
+
+    #--------------------------------------------------------------buscar a quien atacar enenemigos--------------------------------------------------------------
+    #print(atacar)
+    maximo = 0
+    #aQuienAtacar = centroPantalla
+    print(atacar)
+    
+    for distanciaEnemigo in atacar:
+        if(distanciaEnemigo[2] > maximo): 
+            maximo = distanciaEnemigo[2]
+            aQuienAtacar = (distanciaEnemigo[0], distanciaEnemigo[1])
+        
+    #--------------------------------------------------------------HACE FALTA AÑADIR A QUIEN ATACAR MINIONS--------------------------------------------------------------
+    cv2.imshow("webcam", img)
+    cv2.imshow("webcam2", mask)
     cv2.waitKey()
-
-
-atacar = []
-enemigoDetectado = False
-# print(enemigosCoordenadas)
-
-
-py.screenshot().save("hey.png")
-jugador()
-enemigo()
-for coordenada in enemigosCoordenadas:
-    primerPuntoX = coordenada[0]
-    primerPuntoY = coordenada[1]
-    segundoPuntoX = jugadorCoordenada[0]
-    segundoPuntoY = jugadorCoordenada[1]
-
-    distancia = math.sqrt(math.pow(
-        (segundoPuntoX-primerPuntoX), 2) + math.pow((segundoPuntoY-primerPuntoY), 2))
-    # print(str(distancia) + " distancia")
-    if distancia < (550*0.6):
-        cv2.line(img, (primerPuntoX + 50, primerPuntoY + 100),
-                 (segundoPuntoX + 50, segundoPuntoY + 120), (0, 255, 0), 2)
-        atacar.append([primerPuntoX + 50, primerPuntoY + 150, distancia])
-        enemigoDetectado = False
-    else:
-        cv2.line(img, (primerPuntoX + 50, primerPuntoY + 100),
-                 (segundoPuntoX + 50, segundoPuntoY + 120), (0, 0, 255), 2)
-
-# print(atacar)
-maximo = 0
-# aQuienAtacar = centroPantalla
-for distanciaEnemigo in atacar:
-    if (distanciaEnemigo[2] > maximo):
-        maximo = distanciaEnemigo[2]
-        aQuienAtacar = (distanciaEnemigo[0], distanciaEnemigo[1])
-
-
-cv2.imshow("webcam", img)
-cv2.waitKey()
-print("hmm")
-wait(0.5)
-# while enemigoDetectado == False:
-# atacarFuncion()
-'''
-print(aQuienAtacar)
-py.moveTo(aQuienAtacar)
-py.mouseDown(button='right')
-py.mouseUp(button='right')
-print("kitear")
-kitear()
-'''
+    print("hmm")
+    wait(0.5)
+    #while enemigoDetectado == False:
+    #atacarFuncion()
+    print(aQuienAtacar)
+    '''
+    
+    py.moveTo(aQuienAtacar)
+    py.mouseDown(button='right')
+    py.mouseUp(button='right')
+    print("kitear")
+    kitear()
+    '''
